84155d8d6980a53cfc3ca7c33600202ba5d608b8
eaayine-nongre <eugeneatinbire@gmail.com>
2025-04-28 11:48:30 +0000

    Setting up working environment

--- Frontend/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-// src/services/authService.js
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api/auth';
-
-export const login = async (email, password) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
-  return response.data;
-};
